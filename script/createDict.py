d425629ca0bd7aa8d82ee00b51ac2b4df8f12ea1
Matthew Childs <76403824+mcchilds01@users.noreply.github.com>
2021-04-16 16:06:32 -0400

    Add files via upload

--- script/createDict.py
+++ script/createDict.py
@@ -1,6 +1,13 @@
 def createDict(code, country_name, rate):
-	return { 
-		'value': (code, int(float(rate))),
-		'label': code,
-		'xlink': 'https://www.google.com/search?q=covid+infection+rate+in+' + country_name,
+    try: 
+        return {
+            'value': (code, int(float(rate))),
+    		'label': code,
+    		'xlink': 'https://www.google.com/search?q=covid+infection+rate+in+' + country_name,
 		}
+    except: 
+        return { 
+            'value': (code, 0),
+            'label': code,
+            'xlink': 'https://www.google.com/search?q=covid+infection+rate+in+' + country_name,
+          }
