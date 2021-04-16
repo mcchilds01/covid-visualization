d3591ddc20c49b2f48e1f36fbc4f8d31d060c307
Matthew Childs <76403824+mcchilds01@users.noreply.github.com>
2021-04-16 16:08:40 -0400

    Add files via upload

--- script/COVID_19_vis_final.py
+++ script/COVID_19_vis_final.py
@@ -1,9 +1,10 @@
 import pygal 
 import csv
-from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS, RotateStyle
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS, RotateStyle, Style
 from pygal.maps.world import COUNTRIES, World 
 from datetime import date, timedelta
 from country_codes import get_country_code
+# import cairosvg
 from createDict import createDict
 
 filename = 'owid-covid-data.csv'
@@ -15,7 +16,7 @@ with open(filename) as f:
 		# This allows the program to pull the most recent data from OWID, provided the CSV file is updated
 		# if row[3] == str(date.today() - timedelta(1)):
 		# For the purpose of testing against the data in this repo, changed to this:
-		if row[3] == '2020-12-27':
+		if row[3] == str(date.today() - timedelta(1)):
 			code = get_country_code(row[2])
 			if code: 
 				COVID_rates.append(createDict(code, row[2], row[4]))
@@ -28,15 +29,21 @@ with open(filename) as f:
 
 COVID_rates_1 = [i for i in COVID_rates if i['value'][1] < 100000]
 COVID_rates_2 = [country for country in COVID_rates if country['value'][1] < 1000000] 
-COVID_rates_3 = [country for country in COVID_rates if country['value'][1] >= 1000000]
+COVID_rates_3 = [country for country in COVID_rates if country['value'][1] < 10000000]
+COVID_rates_4 = [country for country in COVID_rates if country['value'][1] < 100000000]
+COVID_rates_5 = [country for country in COVID_rates if country['value'][1] >= 100000000]
 
 customstyle = Style(colors=('#008000', '#FFFF00', '#FF9633', '#FF0000', '#800080' ))
 
-wm_style = RotateStyle('#336699', base_style = LCS)
-wm = World(style = customestyle)
-wm.add('< 100000', COVID_rates_1)
-wm.add('< 1000000', COVID_rates_2)
-wm.add('>= 1 mil', COVID_rates_3)
+wm_style = RotateStyle('#336699', step=5) 
+wm = World(style = customstyle)
+wm.add('< 100,000', COVID_rates_1)
+wm.add('< 1,000,000', COVID_rates_2)
+wm.add('< 10,000,000', COVID_rates_3)
+wm.add('< 100,000,000', COVID_rates_4)
+if len(COVID_rates_5) > 1:
+    wm.add('>= 100,000,000', COVID_rates_5)
 # wm.title='Total COVID cases by country as of ' + str(date.today() - timedelta(1))
-wm.title='Total COVID cases by country as of 2020-12-27'
+wm.title='Total COVID cases by country as of ' + str(date.today() - timedelta(1))
 wm.render_to_file('COVID_vis_final.svg')
+# cairosvg.svg2png(url='COVID_vis_final.svg', write_to="'COVID_vis_final.png")
